18c3b7a3bdc55e297dbb7baf7bfb35a84d4dbcf6
Pedro Henrique <80182753+Peedrohenrique@users.noreply.github.com>
2021-05-14 16:28:16 -0300

    projeto desafio V3
    Adicionado pastas do js e css
    e colocando vários códigos no js e no css para organização de todo o conteúdo !

---
+++ js/main.js
@@ -0,0 +1,4 @@
+//***************** INÍCIO DO SCRIPT PARA INDEX **************************
+alert('Bem vindo!!!')
+var nome = prompt('Qual seu nome?')
+//***************** FIM DO SCRIPT PARA INDEX **************************
