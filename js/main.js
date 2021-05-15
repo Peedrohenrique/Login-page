dcf392a48165e2e4e418d6fa0da82a8c251a4daf
Pedro Henrique <80182753+Peedrohenrique@users.noreply.github.com>
2021-05-15 16:40:30 -0300

    Projeto V.5
    finalizado a página da index e tirado o JS que estava em outra página e colocado direto dentro da index;
    
    colocado alguns estilo para finalização da index.

--- js/main.js
+++
@@ -1,4 +0,0 @@
-//***************** INÍCIO DO SCRIPT PARA INDEX **************************
-alert('Bem vindo!!!')
-var nome = prompt('Qual seu nome?')
-//***************** FIM DO SCRIPT PARA INDEX **************************
