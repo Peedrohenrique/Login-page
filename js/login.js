4ed33571afc5062d35bd937e0337452f94aa5156
Pedro Henrique <80182753+Peedrohenrique@users.noreply.github.com>
2021-05-18 10:38:15 -0300

    Meu Projeto V.7.2
    Colocado um sistema de autenticação junto com o JS;
    e corrigido alguns bugs no login.html com a tela do celular!

--- js/login.js
+++ js/login.js
@@ -2,10 +2,10 @@ function logar(){
     var email = document.getElementById("email");
     var senha = document.getElementById("senha");
 
-    if(email.value == "admin@admin.com" && senha.value == "pedro123"){
+    if(email.value == "hendrio" || "paulo" && senha.value == "34970994" || "@123"){
         localStorage.setItem("acesso", true);
         alert("usuário autenticado!")
-        window.location.href = "index.html";
+        window.location.href = "https://www.google.com/";
     } else{
         alert("Usuário ou senha inválidos!")
     }
